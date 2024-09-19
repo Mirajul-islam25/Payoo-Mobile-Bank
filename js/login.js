d810f9d823f0985716cd20c721b1754195310b6f
Mirajul <islammirajul150978@gmail.com>
2024-09-19 23:32:11 +0600

    payoo mobile  bank

--- js/login.js
+++ js/login.js
@@ -6,9 +6,23 @@ document.getElementById('button-login').addEventListener('click', function(event
       //step-1:set preventdefault
       event.preventDefault();
       console.log('button click login');
-      
+
       //step-3:set phn number
       const phoneNumber = document.getElementById('phone-number').value;
       console.log(phoneNumber);
+
+      //step-3:set pin number
+      const pinNumber = document.getElementById('pin-number').value;
+      console.log(pinNumber);
+
+      //bad way to validate
+      if( phoneNumber === '234' && pinNumber === '123456') {
+            console.log('you are logged in');
+            window.location.href ="./js/home-page.html";
+      }
+      else{
+            console.log('wrong phone number or pin number');
+      }
+
 
 })
